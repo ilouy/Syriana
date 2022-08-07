30501eb6fca364f14d1920eebb00def9a080c810
Fady Basem <fadybasem@hotmail.com>
2022-08-07 10:19:12 -0400

    Initial Commit

---
+++ Syriana/SyrianaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyrianaApp.swift
+//  Syriana
+//
+//  Created by Fady Basem on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SyrianaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
